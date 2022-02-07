195a8b683838c74d3658c681d4cd69135f84ebcc
shmd001 <cool.shoronov19@gmail.com>
2022-02-07 16:47:10 +0300

    Add `Message` model

--- base/models.py
+++ base/models.py
@@ -1,3 +1,13 @@
-from django.db import models
+"""BaseApp models"""
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Message(models.Model):
+    """Message model"""
+
+    text = models.CharField(max_length=255)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    # May be IP?
