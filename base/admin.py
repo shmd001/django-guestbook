869b13331385c01feba075695889aee378518655
shmd001 <cool.shoronov19@gmail.com>
2022-02-07 16:51:22 +0300

    Registrate `Message` model

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,6 @@
-from django.contrib import admin
+"""BaseApp admin settings"""
 
-# Register your models here.
+from django.contrib import admin
+from .models import Message
+
+admin.site.register(Message)
