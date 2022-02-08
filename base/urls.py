45dc910a799c0b24a4a60d115e93aac9e50d0ed6
shmd001 <cool.shoronov19@gmail.com>
2022-02-08 13:34:57 +0300

    Add `UserSignup` view

--- base/urls.py
+++ base/urls.py
@@ -1,10 +1,11 @@
 """BaseApp URL Configuration"""
 
 from django.urls import path
-from .views import GuestbookView, UserSignin, UserSignout
+from .views import GuestbookView, UserSignin, UserSignout, UserSignup
 
 urlpatterns = [
     path("", GuestbookView.as_view(), name="guestbook"),
     path("signin/", UserSignin.as_view(), name="signin"),
     path("signout/", UserSignout.as_view(), name="signout"),
+    path("signup/", UserSignup.as_view(), name="signup"),
 ]
--- base/views.py
+++ base/views.py
@@ -1,10 +1,13 @@
 """BaseApp views"""
 
+from django.http import HttpResponseRedirect
 from django.views.generic import ListView
 from django.contrib.auth.views import LoginView, LogoutView
+from django.views.generic.edit import FormView
 from django.urls import reverse_lazy
+from django.contrib.auth import login
 from .models import Message
-from .forms import SigninForm
+from .forms import SigninForm, SignupForm
 
 
 class GuestbookView(ListView):
@@ -28,3 +31,27 @@ class UserSignout(LogoutView):
     """Logout view"""
 
     next_page = reverse_lazy("signin")
+
+
+class UserSignup(FormView):
+    """Register view"""
+
+    form_class = SignupForm
+    success_url = reverse_lazy("guestbook")
+    template_name = "base/signup.html"
+
+    def get(self, request, *args, **kwargs):
+
+        if request.user.is_authenticated:
+            return HttpResponseRedirect(reverse_lazy("guestbook"))
+
+        return super().get(request, *args, **kwargs)
+
+    def form_valid(self, form):
+
+        user = form.save()
+
+        if user is not None:
+            login(self.request, user)
+
+        return super().form_valid(form)
