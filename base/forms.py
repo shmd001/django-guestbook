3b203f25ccc9db95e917375a8ec5325961cc20c5
shmd001 <cool.shoronov19@gmail.com>
2022-02-08 13:34:16 +0300

    Add custom `SignupForm`

--- base/forms.py
+++ base/forms.py
@@ -2,6 +2,7 @@
 
 from django import forms
 from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm
 
 
 class SigninForm(AuthenticationForm):
@@ -35,3 +36,46 @@ class SigninForm(AuthenticationForm):
 
     def __init__(self, request, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
+
+class SignupForm(UserCreationForm):
+    """Custom signup form"""
+
+    username = forms.CharField(
+        max_length=60,
+        min_length=3,
+        label="Username",
+        label_suffix="",
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control rounded-4",
+                "id": "floatingInput",
+            }
+        ),
+    )
+
+    password1 = forms.CharField(
+        max_length=60,
+        min_length=6,
+        label="Password",
+        label_suffix="",
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control rounded-4",
+                "id": "floatingPassword1",
+            }
+        ),
+    )
+
+    password2 = forms.CharField(
+        max_length=60,
+        min_length=6,
+        label="Password confirmation",
+        label_suffix="",
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control rounded-4",
+                "id": "floatingPassword2",
+            }
+        ),
+    )
